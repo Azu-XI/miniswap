0a3f44211cb802b4671496f95bd5fbf494c79183
Azu-XI <238836399+Azu-XI@users.noreply.github.com>
2026-04-09 07:40:47 +0200

    Gear update

--- Azu_30639/MNK.lua
+++ Azu_30639/MNK.lua
@@ -48,6 +48,7 @@ sets.Engaged_Default_Priority = {
         "remove",
     },
     Neck  = {
+        { Name = "Faith Torque", Level = 73 },        -- H2HSkill+7
         { Name = "Peacock Charm", Level = 33 },       --             ACC+10
         { Name = "Spike Necklace", Level = 21 },      -- STR+3 DEX+3
         { Name = "Pile Chain", Level = 3 },
